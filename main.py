8ae611d72da66dccd1f35c64a97026de6cde158c
David Jurado <19822818+djurado@users.noreply.github.com>
2026-06-04 01:42:14 +0000

    secretos

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-
+from config import settings
 app = FastAPI()
 
 @app.get("/hello")
@@ -18,4 +18,12 @@ def sumar(a:int, b:int):
 @app.get("/resta")
 def restar(a:int, b:int):
     return {"resultado": f"{a} - {b} = {a-b}"}
+
+@app.get("/clave_secreta")
+def obtener_clave():
+    return {"clave": f"{settings.clave_secreta.get_secret_value()}"}
+
+@app.get("/titulo_api")
+def obtener_titulo():
+    return {"titulo": f"{settings.title_api}"}
 
