0f95d9967092b51fa284b8ef52a9c2e4989f6936
David Jurado <19822818+djurado@users.noreply.github.com>
2026-06-04 01:01:14 +0000

    Nuevo endpoint root

--- main.py
+++ main.py
@@ -3,5 +3,9 @@ from fastapi import FastAPI
 app = FastAPI()
 
 @app.get("/hello")
-def root():
+def hola_mundo():
     return {"message": "Hola David"}
+
+@app.get("/")
+def root():
+    return {"message": "Bienvenido a mi API"}
